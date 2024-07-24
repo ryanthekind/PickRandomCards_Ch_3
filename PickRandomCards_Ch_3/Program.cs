b0f4a5c286e5d3877f68434defb2e19dd6307c33
Ryan Burns <mr.ryan.burns@outlook.com>
2024-07-23 21:52:49 -0400

    Add project files.

---
+++ PickRandomCards_Ch_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.Write("Enter the number of cards to pick:");
+
+string line = Console.ReadLine();
+
+if (int.TryParse(line, out int numberOfCards))
+{
+    // This block is exectued if line COULD be converted to an int
+    // value that's stored in a new variable called numberOfCards
+}
+else 
+{ 
+    // this block is executed if line COULD NOT be converted to an int
+}
+
