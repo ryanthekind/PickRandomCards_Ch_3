d16935250e47c928d5217a3b9f5381e2ecaf77a8
Ryan Burns <mr.ryan.burns@outlook.com>
2024-08-06 22:25:35 -0400

    working through a problem in the card picker app.

--- PickRandomCards_Ch_3/Program.cs
+++ PickRandomCards_Ch_3/Program.cs
@@ -1,17 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
+using PickRandomCards_Ch_3;
+
 Console.Write("Enter the number of cards to pick:");
 
 string line = Console.ReadLine();
 
+
+// int.TryParase is a special method used to check if the text that the user entered is a number.
 if (int.TryParse(line, out int numberOfCards))
 {
     // This block is exectued if line COULD be converted to an int
     // value that's stored in a new variable called numberOfCards
+    Console.WriteLine(line);
+    Console.WriteLine(numberOfCards);
+
 }
 else 
 { 
-    // this block is executed if line COULD NOT be converted to an int
+    Console.WriteLine("The number was not converted");
+
 }
 
+/*
+
+foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+{
+
+}
+*/
